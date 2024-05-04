6079c250c077e8e8c04a3dee70df0e684563ccee
Julia <zhyvka89@gmail.com>
2024-05-04 22:52:35 +0200

    task 3 in progress

--- task_3/main.py
+++ task_3/main.py
@@ -1,11 +1,13 @@
-from pathlib import Path
 import sys
+from pathlib import Path
+from typing import Counter
 
 
 def parse_log_line(line: str) -> dict:
     date, time, log, *args = line.split()
     msg = ' '.join(args)
     return {'date': date, 'time': time, 'log': log, 'message': msg }
+
 
 def load_logs(file_path: str) -> list:
     if file_path.exists():
@@ -22,11 +24,42 @@ def load_logs(file_path: str) -> list:
     else:
         print("File does not exist.")
 
+
+def filter_logs_by_level(logs: list, level: str) -> list:
+    filter_logs = filter(lambda x: x['log'].lower() == level, logs)
+    filtered_logs_list = list(filter_logs)
+    return list(filtered_logs_list)
+
+
+def count_logs_by_level(logs: list) -> dict:
+    list_of_levels = list()
+    for log in logs:
+        for key, value in log.items():
+            if key == "log":
+                list_of_levels.append(value)
+    logs_count = Counter(list_of_levels)
+    return logs_count
+
+
+def display_log_counts(counts: dict):
+    print(f'{'Log Level':<10} {'|':>5} {'Quantity':>15}')
+    print('--------------------------------------')
+    for key, value in counts.items():
+        print(f'{key:<10} {'|':>5} {value:>10}')
+        
+
 def main():
     dir = sys.argv[1]
     file_path = Path(dir)
     list = load_logs(file_path)
-    print(list)
+    counts = count_logs_by_level(list)
+    display_log_counts(counts)
+    if len(sys.argv) > 2:
+        log_level = sys.argv[2]
+        print(f"Details for log level '{log_level.upper()}'")
+        filtered_logs = filter_logs_by_level(list, log_level)
+        for log in filtered_logs:
+            print(f'{log['date']} {log['time']} - {log['message']}')
 
 if __name__ == "__main__":
     main()
